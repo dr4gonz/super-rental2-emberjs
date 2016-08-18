05d42068795d86f66e51e262ebc11bf83e0dfb9e
Carl Egbert and Matthew Reyes <matthew@>
2016-08-18 15:46:48 -0700

    add computed properties

--- app/components/city-tile.js
+++ app/components/city-tile.js
@@ -5,5 +5,10 @@ export default Ember.Component.extend({
     destroyCity(city) {
     this.sendAction('destroyCity', city);
     },
-  } 
+  },
+  fullLocation: Ember.computed('city.name', 'city.country', function() {
+    return this.get('city.name') +', '+this.get('city.country');
+  }),
+  sortBy: ['cost:asc'],
+  sortedRentals: Ember.computed.sort('city.rentals', 'sortBy'),
 });
